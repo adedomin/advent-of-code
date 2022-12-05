dcf3e70a715239acc5061a7900483de0707720ec
Anthony DeDominic <adedomin@gmail.com>
2022-12-05 18:13:28 -0500

    Day 5 pt 1 & 2

---
+++ y2022/contrib/templates/dayXX.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+type Output = Vec<something>;
+type Solved = i64;
+
+fn parse_input(input: Vec<u8>) -> Output {
+    let tokenizer = AoCTokenizer::new(&input);
+}
+
+fn part1_sol(input: Output) -> Solved  {
+}
+
+fn part2_sol(input: Output) -> Solved{
+}
+
+fn main() -> io::Result<()> {
+    let input = read_input()?;
+    let parsed_input = parse_input(input);
+    let part1 = part1_sol(&parse_input);
+    let part2 = part2_sol(&parse_input);
+    println!("Part1: {part1}, Part2: {part2}");
+    Ok(())
+}
+
