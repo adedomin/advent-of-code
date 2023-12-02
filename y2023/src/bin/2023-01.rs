515ee2b08182e1eafca1fc675539d481f971fef2
adedomin <anthony@dedominic.pw>
2023-12-02 05:42:56 -0500

    2023, day 2 solved, cleaned up day 1

--- y2023/src/bin/2023-01.rs
+++ y2023/src/bin/2023-01.rs
@@ -1,4 +1,4 @@
-use aoc_shared::{array_windows, atoi, destructure_or_none, read_input, Token, Tokenize};
+use aoc_shared::{array_windows, destructure_or_none, read_input, Token, Tokenize};
 use std::io;
 
 fn parse_input_1(input: &[u8]) -> i32 {
@@ -9,9 +9,10 @@ fn parse_input_1(input: &[u8]) -> i32 {
             let d1 = word
                 .iter()
                 .find(|d| d.is_ascii_digit())
-                .expect("some lines do not have a digit");
-            let d2 = word.iter().rfind(|d| d.is_ascii_digit()).unwrap();
-            atoi::<i32, 10>(&[*d1, *d2])
+                .expect("some lines do not have a digit")
+                - b'0';
+            let d2 = word.iter().rfind(|d| d.is_ascii_digit()).unwrap() - b'0';
+            (d1 * 10 + d2) as i32
         })
         .sum()
 }
@@ -42,24 +43,24 @@ fn extract_num(w: &[u8; 5]) -> u8 {
 
 fn extract_num_back(w: &[u8; 5]) -> u8 {
     match w {
-        [b'1', _, _, _, _] => 1,
-        [b'2', _, _, _, _] => 2,
-        [b'3', _, _, _, _] => 3,
-        [b'4', _, _, _, _] => 4,
-        [b'5', _, _, _, _] => 5,
-        [b'6', _, _, _, _] => 6,
-        [b'7', _, _, _, _] => 7,
-        [b'8', _, _, _, _] => 8,
-        [b'9', _, _, _, _] => 9,
-        [b'e', b'n', b'o', _, _] => 1,
-        [b'o', b'w', b't', _, _] => 2,
-        [b'e', b'e', b'r', b'h', b't'] => 3,
-        [b'r', b'u', b'o', b'f', _] => 4,
-        [b'e', b'v', b'i', b'f', _] => 5,
-        [b'x', b'i', b's', _, _] => 6,
-        [b'n', b'e', b'v', b'e', b's'] => 7,
-        [b't', b'h', b'g', b'i', b'e'] => 8,
-        [b'e', b'n', b'i', b'n', _] => 9,
+        [_, _, _, _, b'1'] => 1,
+        [_, _, _, _, b'2'] => 2,
+        [_, _, _, _, b'3'] => 3,
+        [_, _, _, _, b'4'] => 4,
+        [_, _, _, _, b'5'] => 5,
+        [_, _, _, _, b'6'] => 6,
+        [_, _, _, _, b'7'] => 7,
+        [_, _, _, _, b'8'] => 8,
+        [_, _, _, _, b'9'] => 9,
+        [_, _, b'o', b'n', b'e'] => 1,
+        [_, _, b't', b'w', b'o'] => 2,
+        [b't', b'h', b'r', b'e', b'e'] => 3,
+        [_, b'f', b'o', b'u', b'r'] => 4,
+        [_, b'f', b'i', b'v', b'e'] => 5,
+        [_, _, b's', b'i', b'x'] => 6,
+        [b's', b'e', b'v', b'e', b'n'] => 7,
+        [b'e', b'i', b'g', b'h', b't'] => 8,
+        [_, b'n', b'i', b'n', b'e'] => 9,
         _ => 10,
     }
 }
@@ -67,31 +68,29 @@ fn parse_input_2(input: &[u8]) -> i32 {
     input
         .tokenize()
         .flat_map(|t| destructure_or_none!(Token::Something|word| = t))
-        .flat_map(|word| {
-            let mut w1 = word.to_vec();
-            w1.extend([b'z'; 5]);
-            let w1 = array_windows(&w1).find(|w| extract_num(w) != 10);
-            if let Some(w1) = w1 {
-                let w1 = extract_num(w1);
-                let mut w2 = word.iter().rev().copied().collect::<Vec<u8>>();
-                w2.extend([b'z'; 5]);
-                let w2 = extract_num_back(
-                    array_windows(&w2)
-                        .find(|w| extract_num_back(w) != 10)
-                        .unwrap(),
-                );
-                Some((w1 * 10 + w2) as i32)
-            } else {
-                None
-            }
+        .map(|word| {
+            let mut word_pad = vec![0u8; 4];
+            word_pad.extend(word);
+            word_pad.extend([0u8; 4]);
+            let w1 = extract_num(
+                array_windows(&word_pad)
+                    .find(|w| extract_num(w) != 10)
+                    .expect("to find one number"),
+            );
+            let w2 = extract_num_back(
+                array_windows(&word_pad)
+                    .rfind(|w| extract_num_back(w) != 10)
+                    .unwrap(),
+            );
+            (w1 * 10 + w2) as i32
         })
         .sum()
 }
 
 fn main() -> io::Result<()> {
     let input = read_input()?;
-    // let part1 = parse_input_1(&input);
+    let part1 = parse_input_1(&input);
     let part2 = parse_input_2(&input);
-    println!("Part1: {{part1}}, Part2: {part2}");
+    println!("Part1: {part1}, Part2: {part2}");
     Ok(())
 }
---
+++ y2023/src/bin/2023-02.rs
@@ -0,0 +1,88 @@
+use aoc_shared::{read_input, try_atoi, Token, Tokenize};
+use std::io;
+
+type Output = Vec<Vec<Game>>;
+
+#[derive(Default, Debug)]
+struct Game {
+    r: i32,
+    g: i32,
+    b: i32,
+}
+
+fn parse_input(input: &[u8]) -> Output {
+    let mut ret = vec![];
+    let mut cur_num = 0;
+    for token in input.tokenize() {
+        match token {
+            Token::Something(b"Game") => {
+                ret.push(vec![Game::default()]);
+            }
+            Token::Something(b"red") => ret.last_mut().unwrap().last_mut().unwrap().r = cur_num,
+            Token::Something(b"green") => ret.last_mut().unwrap().last_mut().unwrap().g = cur_num,
+            Token::Something(b"blue") => ret.last_mut().unwrap().last_mut().unwrap().b = cur_num,
+            Token::Something(num) => {
+                cur_num = try_atoi::<i32, 10>(num).unwrap_or(cur_num);
+            }
+            Token::Delimiter(b';') => ret.last_mut().unwrap().push(Game::default()),
+            _ => (),
+        }
+    }
+
+    ret
+}
+
+fn glecmp(lhs: &Game, rhs: &Game) -> bool {
+    lhs.r <= rhs.r && lhs.g <= rhs.g && lhs.b <= rhs.b
+}
+
+const P1MAX_GAME: Game = Game {
+    r: 12,
+    g: 13,
+    b: 14,
+};
+
+fn solve1(i: &[Vec<Game>]) -> i32 {
+    i.iter()
+        .enumerate()
+        .map(|(pos, games)| {
+            if games.iter().all(|game| glecmp(game, &P1MAX_GAME)) {
+                (pos + 1) as i32
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn gmax(lhs: &Game, rhs: &Game) -> Game {
+    Game {
+        r: std::cmp::max(lhs.r, rhs.r),
+        g: std::cmp::max(lhs.g, rhs.g),
+        b: std::cmp::max(lhs.b, rhs.b),
+    }
+}
+
+fn gpower(i: Game) -> i32 {
+    i.r * i.g * i.b
+}
+
+fn solve2(i: &[Vec<Game>]) -> i32 {
+    i.iter()
+        .map(|games| {
+            games
+                .iter()
+                .fold(Game::default(), |acc, game| gmax(&acc, game))
+        })
+        .map(|game| gpower(game))
+        .sum()
+}
+
+fn main() -> io::Result<()> {
+    let input = read_input()?;
+    let parsed_input = parse_input(&input);
+    let part1 = solve1(&parsed_input);
+    let part2 = solve2(&parsed_input);
+    println!("Part1: {part1}, Part2: {part2}");
+    Ok(())
+}
