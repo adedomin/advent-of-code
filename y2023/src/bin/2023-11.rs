b4fcb8fc83ff46f7e1cf0a2c979c5bea86699f0b
Anthony DeDominic <adedomin@gmail.com>
2023-12-11 17:25:55 -0500

    faster day11

--- y2023/src/bin/2023-11.rs
+++ y2023/src/bin/2023-11.rs
@@ -1,30 +1,23 @@
-use aoc_shared::{parse_to_flat2d, read_input, FlatVec2D};
-use itertools::Itertools;
+use aoc_shared::{array_windows, parse_to_flat2d, read_input, FlatVec2D};
 use std::io;
 
-fn expand_universe(
-    (x, y): (i64, i64),
-    horizon: &[i64],
-    vertical: &[i64],
-    expansion_factor: i64,
-) -> (i64, i64) {
-    let dv = (expansion_factor - 1) * vertical[x as usize];
-    let dh = (expansion_factor - 1) * horizon[y as usize];
-
-    (x + dv, y + dh)
+// find geometric median, input is sorted by order of traversal.
+fn geomed(points: &[i64]) -> i64 {
+    array_windows(points)
+        .enumerate()
+        .map(|(p, [xy1, xy2])| {
+            let pos = p as i64;
+            let max = points.len() as i64;
+            xy1.abs_diff(*xy2) as i64 * (pos + 1) as i64 * (max - pos - 1) as i64
+        })
+        .sum()
 }
 
-fn adjust_pairs(p1: (i64, i64), p2: (i64, i64)) -> ((i64, i64), (i64, i64)) {
-    if p1 < p2 {
-        (p1, p2)
-    } else {
-        (p2, p1)
-    }
-}
-
-fn parse(map: &FlatVec2D<u8>, expansion_factor: i64) -> Vec<((i64, i64), (i64, i64))> {
-    let mut galaxies = vec![];
-    let mut hori = vec![0; map.1];
+// fulfills ask of sum of all minimum distances between points via geo median.
+// properties of manhattan distance allows for an O(n) sum of of all the X's and Y's distance
+// in the map.
+fn parse_and_solve(map: &FlatVec2D<u8>, expansion_factor: i64) -> i64 {
+    let mut ypoints = vec![];
     let mut last = 0;
     for y in 0..map.2 {
         let mut found = false;
@@ -32,16 +25,15 @@ fn parse(map: &FlatVec2D<u8>, expansion_factor: i64) -> Vec<((i64, i64), (i64, i
             let chr = map[(x, y)];
             if chr == b'#' {
                 found = true;
-                galaxies.push((x as i64, y as i64));
+                ypoints.push(y as i64 + last);
             }
         }
         if !found {
-            last += 1;
+            last += expansion_factor - 1;
         }
-        hori[y] = last;
     }
 
-    let mut vert = vec![0; map.2];
+    let mut xpoints = vec![];
     last = 0;
     for x in 0..map.1 {
         let mut found = false;
@@ -49,41 +41,25 @@ fn parse(map: &FlatVec2D<u8>, expansion_factor: i64) -> Vec<((i64, i64), (i64, i
             let chr = map[(x, y)];
             if chr == b'#' {
                 found = true;
+                xpoints.push(x as i64 + last);
             }
         }
         if !found {
-            last += 1;
+            last += expansion_factor - 1;
         }
-        vert[x] = last;
     }
 
-    let galpair = galaxies
-        .into_iter()
-        .map(|gal| expand_universe(gal, &hori, &vert, expansion_factor))
-        .permutations(2)
-        .map(|perm| adjust_pairs(perm[0], perm[1]))
-        .unique()
-        .collect::<Vec<((i64, i64), (i64, i64))>>();
-    galpair
-}
-
-fn solve(galaxy_pairs: Vec<((i64, i64), (i64, i64))>) -> u64 {
-    galaxy_pairs
-        .into_iter()
-        .map(|((x1, y1), (x2, y2))| x1.abs_diff(x2) + y1.abs_diff(y2))
-        .sum()
+    geomed(&ypoints) + geomed(&xpoints)
 }
 
 fn main() -> io::Result<()> {
     let input = read_input()?;
     let map = parse_to_flat2d(&input);
 
-    let pair1 = parse(&map, 2);
-    let part1 = solve(pair1);
+    let part1 = parse_and_solve(&map, 2);
     print!("Part1: {part1}, ");
 
-    let pair2 = parse(&map, 1_000_000);
-    let part2 = solve(pair2);
+    let part2 = parse_and_solve(&map, 1_000_000);
     print!("Part2: {part2}");
     println!();
     Ok(())
