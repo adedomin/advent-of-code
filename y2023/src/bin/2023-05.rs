2fef8ab684a15a2e38cccd26b622a9364f4e1eaf
Anthony DeDominic <adedomin@gmail.com>
2023-12-13 18:58:16 -0500

    better part2 solution that is not brutish

--- y2023/src/bin/2023-05.rs
+++ y2023/src/bin/2023-05.rs
@@ -1,19 +1,8 @@
-#![feature(btree_cursors)]
-#![feature(array_chunks)]
 use aoc_shared::{atoi, destructure_or_none, read_input, try_atoi, GroupTokenize, Token};
 use itertools::Itertools;
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
-use std::{
-    collections::BTreeMap,
-    io,
-    ops::{Bound, Range},
-};
+use std::{io, ops::Range};
 
-type Output = (
-    Vec<i64>,
-    Vec<BTreeMap<i64, (i64, i64)>>,
-    Vec<BTreeMap<i64, (i64, i64)>>,
-);
+type Output = (Vec<i64>, Vec<Vec<(Range<i64>, i64)>>);
 
 fn parse_input(input: &[u8]) -> Output {
     let mut iter = input.group_tokens(Token::DoubleNewline);
@@ -25,96 +14,81 @@ fn parse_input(input: &[u8]) -> Output {
         .map(|w| atoi::<i64, 10>(w))
         .collect::<Vec<i64>>();
 
-    let mut forward = vec![];
-    let mut reverse = vec![];
-
-    iter.for_each(|toks| {
-        let mut ftree = BTreeMap::new();
-        let mut rtree = BTreeMap::new();
-        toks.iter()
-            .flat_map(|t| destructure_or_none!(Token::Something|word| = t))
-            .flat_map(|t| try_atoi::<i64, 10>(t))
-            .tuples()
-            .for_each(|(dst, src, to)| {
-                ftree.insert(src, (src + to, dst));
-                rtree.insert(dst, (dst + to, src));
-            });
-        forward.push(ftree);
-        reverse.push(rtree);
-    });
-    (
-        seedlist,
-        forward,
-        reverse.iter().rev().cloned().collect::<Vec<_>>(),
-    )
-}
-
-fn descend_alma(seed: i64, almanac: &[BTreeMap<i64, (i64, i64)>]) -> i64 {
-    let mut last = seed;
-    for map in almanac {
-        let b = map.upper_bound(Bound::Included(&last));
-        if let Some((src, (srcto, dst))) = b.key_value() {
-            if *srcto > last {
-                let diff = last.abs_diff(*src) as i64;
-                last = *dst + diff;
-            }
-        }
-    }
-    last
-}
-
-fn bottom_up_alma(
-    seed: i64,
-    almanac: &[BTreeMap<i64, (i64, i64)>],
-    seed_interval: &[Range<i64>],
-) -> Option<i64> {
-    let nseed = descend_alma(seed, almanac);
-    if seed_interval
-        .iter()
-        .find(|iv| iv.contains(&nseed))
-        .is_some()
-    {
-        Some(nseed)
-    } else {
-        None
-    }
-}
-
-fn lowest_seed(seeds: &[i64], almanac: &[BTreeMap<i64, (i64, i64)>]) -> i64 {
-    seeds
-        .iter()
-        .copied()
-        .map(|seed| descend_alma(seed, almanac))
-        .min()
-        .unwrap()
-}
-
-fn solve_interval(seeds: &[i64], almanac: &[BTreeMap<i64, (i64, i64)>]) -> i64 {
-    let seed_interv = seeds
-        .array_chunks()
-        .map(|&[start, end]| {
-            let end = start + end;
-            start..end
+    let intervals = iter
+        .map(|toks| {
+            toks.iter()
+                .flat_map(|t| destructure_or_none!(Token::Something|word| = t))
+                .flat_map(|t| try_atoi::<i64, 10>(t))
+                .tuples()
+                .map(|(dst, src, to)| (src..src + to, dst - src))
+                .collect_vec()
         })
-        .collect::<Vec<_>>();
-    let biggest = seed_interv
-        .iter()
-        .max_by(|ix, iy| ix.end.cmp(&iy.end))
-        .unwrap()
-        .end;
-    (0..biggest)
-        .into_par_iter()
-        .find_first(|&backward_v| bottom_up_alma(backward_v, almanac, &seed_interv).is_some())
-        .unwrap()
+        .collect_vec();
+    (seedlist, intervals)
+}
+
+fn solve_interval(mut seeds: Vec<(bool, Range<i64>)>, almanac: &[Vec<(Range<i64>, i64)>]) -> i64 {
+    for alma in almanac {
+        let mut new_ranges = vec![];
+        for (iv, off) in alma {
+            let mut read_unchanged = vec![];
+            seeds
+                .iter_mut()
+                .filter(|(a, siv)| *a && (iv.start < siv.end && siv.start < iv.end))
+                .for_each(|(active, siv)| {
+                    *active = false;
+                    let mut ns = iv.start;
+                    if siv.start < iv.start {
+                        read_unchanged.push((true, siv.start..iv.start));
+                    } else {
+                        ns = siv.start;
+                    }
+
+                    if siv.end <= iv.end {
+                        new_ranges.push((true, ns + off..siv.end + off));
+                    } else {
+                        new_ranges.push((true, ns + off..iv.end + off));
+                        read_unchanged.push((true, iv.end..siv.end));
+                    }
+                });
+            seeds.extend(read_unchanged);
+        }
+        seeds = seeds
+            .into_iter()
+            .filter(|(a, _)| *a)
+            .chain(new_ranges)
+            .sorted_by_key(|(_, i)| i.start)
+            .collect_vec();
+    }
+
+    seeds
+        .into_iter()
+        .filter(|(a, _)| *a)
+        .map(|(_, s)| s.start)
+        .sorted()
+        .next()
+        .expect("at least one seed.")
 }
 
 // fn part2_sol(input: &Output) -> Solved {}
 
 fn main() -> io::Result<()> {
     let input = read_input()?;
-    let (init_seed, almanac, rev_alma) = parse_input(&input);
-    let part1 = lowest_seed(&init_seed, &almanac);
-    let part2 = solve_interval(&init_seed, &rev_alma);
+    let (init_seed, almanac) = parse_input(&input);
+
+    let interval_1 = init_seed
+        .iter()
+        .map(|&seed| (true, seed..seed + 1))
+        .collect_vec();
+    let part1 = solve_interval(interval_1, &almanac);
+
+    let interval_2 = init_seed
+        .iter()
+        .tuples()
+        .map(|(&seed1, &seed2)| (true, seed1..seed1 + seed2))
+        .collect_vec();
+    let part2 = solve_interval(interval_2, &almanac);
+
     println!("Part1: {part1}, Part2: {part2}");
     Ok(())
 }
