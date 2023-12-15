0007e736409496e83ec2805c1585ba9bd0d4b561
Anthony DeDominic <adedomin@gmail.com>
2023-12-15 08:49:27 -0500

    2023-15 p1 & 2

---
+++ y2023/src/bin/2023-15.rs
@@ -0,0 +1,62 @@
+use aoc_shared::{atoi, read_input_to_string, Tokenize};
+use std::io;
+
+fn HASH_hasher(v: &[u8]) -> usize {
+    v.iter()
+        .fold(0, |acc, &chr| (acc + chr as usize) * 17 % 256)
+}
+
+fn parse_solve(input: &str) -> usize {
+    input.split(',').map(|s| HASH_hasher(s.as_bytes())).sum()
+}
+
+fn solve2(input: &[u8]) -> usize {
+    input
+        .tokenize()
+        .fold(
+            (vec![vec![]; 256], b"".as_slice()),
+            |(mut acc, clabel), tok| match tok {
+                aoc_shared::Token::Something(label) if clabel.is_empty() => (acc, label),
+                aoc_shared::Token::Something(focal) => {
+                    let idx = HASH_hasher(clabel);
+                    let focal = atoi::<u8, 10>(focal);
+                    if let Some(found) = acc[idx].iter_mut().find(|(l, _)| *l == clabel) {
+                        *found = (found.0, focal);
+                    } else {
+                        acc[idx].push((clabel, focal));
+                    }
+                    (acc, b"")
+                }
+                aoc_shared::Token::Delimiter(b'-') => {
+                    let idx = HASH_hasher(clabel);
+                    acc[idx] = acc[idx]
+                        .drain(..)
+                        .filter(|(label, _)| *label != clabel)
+                        .collect::<Vec<_>>();
+                    (acc, b"")
+                }
+                _ => (acc, clabel),
+            },
+        )
+        .0
+        .into_iter()
+        .enumerate()
+        .filter(|(_, b)| !b.is_empty())
+        .map(|(box_num, b)| {
+            b.iter()
+                .enumerate()
+                .map(|(pos, (_, focal))| (box_num + 1) * (pos + 1) * (*focal as usize))
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn main() -> io::Result<()> {
+    let input = read_input_to_string()?;
+    let part1 = parse_solve(input.trim_end());
+    let part2 = solve2(input.as_bytes());
+    print!("Part1: {part1}, ");
+    print!("Part2: {part2}");
+    println!();
+    Ok(())
+}
