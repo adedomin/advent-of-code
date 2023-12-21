26fe5feaaade67c2c9205e5e64111ecf20fec814
Anthony DeDominic <adedomin@gmail.com>
2023-12-21 03:41:08 -0500

    part 2

--- y2023/src/bin/2023-20.rs
+++ y2023/src/bin/2023-20.rs
@@ -13,68 +13,16 @@ use std::{
     io,
 };
 
-/* copy paste from day 8 */
-const PRIMES_TO_1009: [u32; 169] = [
-    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
-    101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
-    197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307,
-    311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421,
-    431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547,
-    557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
-    661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-    809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929,
-    937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009,
-];
-
-fn factorize(n: u32) -> Vec<(usize, u32)> {
-    let mut ret = [0u32; PRIMES_TO_1009.len()];
-    let mut i = 0;
-    let mut n = n;
-    while n > 1 {
-        if n % PRIMES_TO_1009[i] == 0 {
-            n /= PRIMES_TO_1009[i];
-            ret[i] += 1;
-        } else {
-            i += 1;
-        }
-    }
-
-    ret.iter()
-        .copied()
-        .enumerate()
-        .filter(|(_, cnt)| *cnt > 0)
-        .collect::<Vec<_>>()
-}
-
-fn least_common_mult(factors: Vec<(usize, u32)>) -> u64 {
-    let mut resi = [0u32; PRIMES_TO_1009.len()];
-
-    for (prime, cnt) in factors {
-        let curr = resi[prime];
-        resi[prime] = std::cmp::max(cnt, curr);
-    }
-
-    resi.iter()
-        .enumerate()
-        .filter(|(_, &cnt)| cnt != 0)
-        .map(|(p, pow)| {
-            let prime = PRIMES_TO_1009[p];
-            prime.pow(*pow) as u64
-        })
-        .product()
-}
-/* end paste from day 8 */
-
 type Label = [u8; 2];
 
-#[derive(Eq, PartialEq, Clone, Debug)]
+#[derive(Eq, PartialEq, Clone)]
 enum CType {
     Broadcast(bool),
     FlipFlop(bool),
     Conjunction(Vec<(Label, Energy)>),
 }
 
-#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+#[derive(Eq, PartialEq, Clone, Copy)]
 enum Energy {
     High,
     Low,
@@ -90,7 +38,7 @@ impl From<u8> for CType {
     }
 }
 
-#[derive(Eq, PartialEq, Clone, Debug)]
+#[derive(Eq, PartialEq, Clone)]
 struct Circuit(pub CType, pub Label);
 
 impl Circuit {
@@ -219,12 +167,12 @@ const BROADCAST: Label = [b'Z', b'Z'];
 const BUTTON: Label = [b'Z', b'Y'];
 const RX: Label = [b'r', b'x'];
 
-fn cycle(input: &mut DesertMachine, checked: &[Label]) -> (u64, u64, Vec<Energy>) {
+fn cycle(input: &mut DesertMachine, checked: &[Label]) -> (u64, u64, Vec<bool>) {
     let mut low = 0u64;
     let mut high = 0u64;
     let mut queue = VecDeque::from([(Energy::Low, BROADCAST, BUTTON)]);
-    let mut checked_v = vec![Energy::Low; checked.len()];
-    while let Some((energy, circuit_id, activated_by)) = queue.pop_front() {
+    let mut checked_v = vec![false; checked.len()];
+    while let Some((energy, circuit_id, activated_by)) = queue.pop_back() {
         match energy {
             Energy::High => high += 1,
             Energy::Low => low += 1,
@@ -241,7 +189,9 @@ fn cycle(input: &mut DesertMachine, checked: &[Label]) -> (u64, u64, Vec<Energy>
                 }
                 // look at watched circuits and detect the energies they emitted.
                 if let Some(pos) = checked.iter().position(|c| *c == circuit_id) {
-                    checked_v[pos] = next_e;
+                    if matches!(next_e, Energy::High) {
+                        checked_v[pos] = true;
+                    }
                 }
             }
         }
@@ -260,12 +210,8 @@ fn solve1(mut input: DesertMachine) -> u64 {
     low * high
 }
 
-fn print_label(i: &Label) {
-    println!("{}", std::str::from_utf8(i).unwrap());
-}
-
 fn solve2(mut input: DesertMachine, inverse: InverseMap) -> u64 {
-    let mut i = 0u32;
+    let mut i = 0u64;
     // we need to go one level above rx's dep because of how conjugators work...
     // we assume looking one level gets the cycle based on a visualization of
     // the input's cycles and connections..... is not generalized.
@@ -274,13 +220,8 @@ fn solve2(mut input: DesertMachine, inverse: InverseMap) -> u64 {
         .expect("rx should be mapped")
         .iter()
         .flat_map(|v| inverse.get(v).unwrap())
-        .flat_map(|v| inverse.get(v).unwrap())
-        .flat_map(|v| inverse.get(v).unwrap())
         .copied()
         .collect::<Vec<Label>>();
-    for w in rx_watches.iter() {
-        print_label(w);
-    }
     let mut rx_s = vec![0; rx_watches.len()];
     while !rx_s.iter().all(|v| *v != 0) {
         i += 1;
@@ -289,16 +230,12 @@ fn solve2(mut input: DesertMachine, inverse: InverseMap) -> u64 {
             .iter()
             .enumerate()
             .for_each(|(pos, e)| {
-                if matches!(e, Energy::High) {
+                if *e && rx_s[pos] == 0 {
                     rx_s[pos] = i;
                 }
             });
     }
-    least_common_mult(
-        rx_s.into_iter()
-            .flat_map(factorize)
-            .collect::<Vec<(usize, u32)>>(),
-    )
+    rx_s.into_iter().product()
 }
 
 fn main() -> io::Result<()> {
