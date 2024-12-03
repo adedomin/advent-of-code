95a730d1a6c04b9545f3e61df18e0f0680ee8f38
Anthony DeDominic <adedomin@gmail.com>
2024-12-03 06:52:31 -0500

    part 1 & 2, 2024-03

---
+++ y2024/src/bin/2024-03.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+use aoc_shared::{fold_decimal_from, read_input};
+use regex::bytes::Regex;
+
+type Output = i64;
+
+fn parse_solve1(input: &[u8]) -> (Output, Output) {
+    let re = Regex::new(
+        r##"(?x)
+         (?<do>do\(\))
+        |(?<dont>don't\(\))
+        |(?:mul\((?<n1>-?[0-9]+),(?<n2>-?[0-9]+)\))
+    "##,
+    )
+    .unwrap();
+    let (p1, p2, _) =
+        re.captures_iter(input)
+            .fold((0, 0, true), |(acc, acc2, enabled), matcher| {
+                if matcher.name("do").is_some() {
+                    (acc, acc2, true)
+                } else if matcher.name("dont").is_some() {
+                    (acc, acc2, false)
+                } else {
+                    let n1 = fold_decimal_from::<Output>(matcher.name("n1").unwrap().as_bytes());
+                    let n2 = fold_decimal_from::<Output>(matcher.name("n2").unwrap().as_bytes());
+                    let prod = n1 * n2;
+                    (
+                        acc + prod,
+                        if enabled { acc2 + prod } else { acc2 },
+                        enabled,
+                    )
+                }
+            });
+    (p1, p2)
+}
+
+fn main() -> io::Result<()> {
+    let input = read_input()?;
+    let (part1, part2) = parse_solve1(&input);
+    print!("Part1: {part1}, ");
+    print!("Part2: {part2}");
+    println!();
+    Ok(())
+}
