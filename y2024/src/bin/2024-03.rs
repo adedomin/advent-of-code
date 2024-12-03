ccf0de8c295f4931c3b30abc5db96f65d4dd941b
Anthony DeDominic <adedomin@gmail.com>
2024-12-03 13:36:26 -0500

    one println

--- y2024/src/bin/2024-03.rs
+++ y2024/src/bin/2024-03.rs
@@ -36,8 +36,6 @@ fn parse_solve1(input: &[u8]) -> (Output, Output) {
 fn main() -> io::Result<()> {
     let input = read_input()?;
     let (part1, part2) = parse_solve1(&input);
-    print!("Part1: {part1}, ");
-    print!("Part2: {part2}");
-    println!();
+    println!("Part1: {part1}, Part2: {part2}");
     Ok(())
 }
