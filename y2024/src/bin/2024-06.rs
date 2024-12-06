83054a9a4c751f65bb6cfc5e5b0d7eb14103dd65
Anthony DeDominic <adedomin@gmail.com>
2024-12-06 17:27:23 -0500

    use vector that uses a bitset for where in the rotation we have visited a specific area, speeds up cycle det as well.

--- y2024/src/bin/2024-06.rs
+++ y2024/src/bin/2024-06.rs
@@ -1,6 +1,5 @@
-use aoc_shared::{pad_to_flat2d, read_input, FlatVec2D};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use rustc_hash::FxHashSet;
+use aoc_shared::{flat_coord, inverse_flat_coord, pad_to_flat2d, read_input, FlatVec2D};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 use std::io;
 
 #[derive(Default, Copy, Clone)]
@@ -29,29 +28,32 @@ type Solved = usize;
 // right turn (clockwise 90deg)
 // note that cos(90deg) is always ~0
 // note that sin(90deg) is always ~1
-fn rot_right_90(x: isize, y: isize) -> (isize, isize) {
+const fn rot_right_90((x, y): (isize, isize)) -> (isize, isize) {
     (-y, x)
 }
+const ROT_TAB: [(isize, isize); 4] = {
+    let mut ret = [(0, -1); 4];
+    let mut i = 1;
+    while i < ret.len() {
+        ret[i] = rot_right_90(ret[i - 1]);
+        i += 1;
+    }
+    ret
+};
 
 fn move_next(x: isize, dx: isize, y: isize, dy: isize) -> (usize, usize) {
     ((x + dx) as usize, (y + dy) as usize)
 }
 
-fn patrol(
-    map: &Output,
-    (sx, sy): (isize, isize),
-    njxy: (isize, isize),
-) -> Option<FxHashSet<(isize, isize)>> {
-    let (mut x, mut dx, mut y, mut dy) = (sx, 0, sy, -1);
-    let mut visited = FxHashSet::default();
+fn patrol(map: &Output, (sx, sy): (isize, isize), njxy: (isize, isize)) -> Option<Vec<u8>> {
+    let mut rot_ind = 0u8;
+    let (mut dx, mut dy) = ROT_TAB[rot_ind as usize];
+    let (mut x, mut y) = (sx, sy);
+    let mut visited = vec![0; map.1 * map.2];
     if (sx, sy) == njxy {
         return Some(visited); // can't start here so not a valid loop
     }
-    let mut cycle_det = FxHashSet::default();
-    while !matches!(map[(x, y)], X::Out) {
-        if !visited.insert((x, y)) && !cycle_det.insert((x, dx, y, dy)) {
-            return None; // contains a cycle.
-        }
+    loop {
         /* Handle a case where we may turn 180deg
            e.g.:
 
@@ -62,13 +64,19 @@ fn patrol(
         for _ in 0..2 {
             let (mx, my) = move_next(x, dx, y, dy);
             if (mx as isize, my as isize) == njxy || matches!(map[(mx, my)], X::Junk) {
-                (dx, dy) = rot_right_90(dx, dy);
+                rot_ind = (rot_ind + 1) % 4;
+                (dx, dy) = ROT_TAB[rot_ind as usize];
             } else {
                 break;
             }
         }
         x += dx;
         y += dy;
+        if matches!(map[(x, y)], X::Out) {
+            break;
+        }
+        let vmap = flat_coord(x as usize, y as usize, map.1);
+        (visited[vmap] & (1 << rot_ind) == 0).then(|| visited[vmap] |= 1 << rot_ind)?;
     }
     Some(visited)
 }
@@ -86,11 +94,16 @@ fn solve(map: &Output) -> (Solved, Solved) {
 
     if let Some((x, y)) = pos {
         let part1_visit = patrol(map, (x, y), (-1, -1)).expect("input should be cycle free");
-        let p1 = part1_visit.len();
+        let p1 = part1_visit.iter().filter(|&&v| v != 0).count();
         // sum of all cycling inputs possible from a given visit map.
         let p2 = part1_visit
             .into_par_iter()
-            .map(|(njx, njy)| usize::from(patrol(map, (x, y), (njx, njy)).is_none()))
+            .enumerate()
+            .filter(|(_, v)| *v != 0)
+            .map(|(idx, _)| {
+                let (njx, njy) = inverse_flat_coord(idx, map.1);
+                usize::from(patrol(map, (x, y), (njx as isize, njy as isize)).is_none())
+            })
             .sum();
         (p1, p2)
     } else {
