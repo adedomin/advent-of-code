e3f2710c67360fcdecf4806fcf4e7a1209b3f516
Anthony DeDominic <adedomin@gmail.com>
2024-12-09 15:53:03 -0500

    more code changes because i am insane.

--- y2024/src/bin/2024-09.rs
+++ y2024/src/bin/2024-09.rs
@@ -1,108 +1,102 @@
 use aoc_shared::read_input;
-use std::io;
+use std::{io, iter};
 
-type Output = Vec<(DiskUse, usize, usize)>;
-type Id = usize;
+type UsedList = Vec<Use>;
+type Output = (Vec<DiskUse>, UsedList, UsedList);
 
-#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+#[derive(Clone, Copy)]
 enum DiskUse {
     Free,
-    Used(Id),
+    Used(usize),
+}
+
+#[derive(Clone, Copy)]
+struct Use {
+    id: usize,
+    start: usize,
+    end: usize,
 }
 
 fn parse_input(input: &[u8]) -> Output {
-    let mut id = 0;
-    let mut totlen = 0;
+    let mut disk = vec![];
+    let mut free = vec![];
+    let mut used = vec![];
     input
         .iter()
         .filter(|d| d.is_ascii_digit())
         .enumerate()
-        .map(|(start, sz)| {
+        .for_each(|(start, sz)| {
             let len = (sz - b'0') as usize;
-            let r = if start & 1 == 0 {
-                let r = (DiskUse::Used(id), totlen, totlen + len);
-                id += 1;
-                r
+            if start % 2 == 0 {
+                let id = start / 2;
+                let start = disk.len();
+                disk.extend(iter::repeat_n(DiskUse::Used(id), len));
+                used.push(Use {
+                    id,
+                    start,
+                    end: disk.len(),
+                });
             } else {
-                (DiskUse::Free, totlen, totlen + len)
+                let start = disk.len();
+                disk.extend(iter::repeat_n(DiskUse::Free, len));
+                free.push(Use {
+                    id: 0,
+                    start,
+                    end: disk.len(),
+                });
             };
-            totlen += len;
-            r
-        })
-        .collect::<Output>()
+        });
+    (disk, free, used)
 }
 
-fn part1_sol(mut input: Output) -> Id {
+fn part1_sol(input: Vec<DiskUse>) -> usize {
     let mut ans = 0;
-    let mut i = 1; // we can skip the first (always zero and filled)
+    let mut i = 0;
     let mut j = input.len() - 1;
     while i <= j {
         match input[i] {
-            (DiskUse::Free, start, len) => {
+            DiskUse::Free => loop {
                 match input[j] {
-                    (DiskUse::Free, _, _) => j -= 1,
-                    (DiskUse::Used(id), s, l) => {
-                        let min = if (l - s) >= (len - start) {
-                            len - start
-                        } else {
-                            l - s
-                        };
-                        for s in start..start + min {
-                            ans += s * id;
-                            // use up spaces
-                        }
-                        input[i].1 += min;
-                        input[j].2 -= min;
-                        if input[i].1 == input[i].2 {
-                            i += 1;
-                        }
-                        if input[j].1 == input[j].2 {
-                            j -= 1;
-                        }
+                    DiskUse::Free => j -= 1,
+                    DiskUse::Used(id) => {
+                        ans += i * id;
+                        j -= 1;
+                        break;
                     }
-                };
-            }
-            (DiskUse::Used(id), start, len) => {
-                (start..len).for_each(|idx| ans += idx * id);
-                input[i].2 -= len - start;
-                i += 1;
-            }
+                }
+            },
+            DiskUse::Used(id) => ans += i * id,
         }
+        i += 1;
     }
     ans
 }
 
-type FreeList = Vec<(usize, usize)>;
-type UsedList = Vec<(Id, usize, usize)>;
-
-fn part2_sol(input: Output) -> Id {
+fn part2_sol(mut freelist: UsedList, usedlist: UsedList) -> usize {
     let mut ans = 0;
-    let mut freelist: FreeList = Vec::with_capacity(input.len());
-    let mut usedlist: UsedList = Vec::with_capacity(input.len());
-    input.into_iter().for_each(|(d, s, l)| match d {
-        DiskUse::Free => freelist.push((s, l)),
-        DiskUse::Used(id) => usedlist.push((id, s, l)),
-    });
-
-    usedlist.into_iter().rev().for_each(|(id, us, ul)| {
-        if let Some((fstart, _)) = freelist
-            .iter_mut()
-            .find(|(fs, fl)| (ul - us) <= (fl - fs) && *fs < us)
-        {
-            (0..(ul - us)).for_each(|idx| ans += (*fstart + idx) * id);
-            *fstart += ul - us;
-        } else {
-            (us..ul).for_each(|idx| ans += idx * id);
-        }
-    });
+    usedlist
+        .into_iter()
+        .rev()
+        .for_each(|Use { id, start, end }| {
+            if let Some(Use { start: fstart, .. }) = freelist.iter_mut().find(
+                |Use {
+                     start: fs, end: fe, ..
+                 }| (end - start) <= (fe - fs) && *fs < start,
+            ) {
+                (0..(end - start)).for_each(|idx| ans += (*fstart + idx) * id);
+                *fstart += end - start;
+            } else {
+                (start..end).for_each(|idx| ans += idx * id);
+            }
+        });
     ans
 }
 
 fn main() -> io::Result<()> {
     let input = read_input()?;
-    let parsed_input = parse_input(&input);
-    let part1 = part1_sol(parsed_input.clone());
-    let part2 = part2_sol(parsed_input);
+    let (disk, freelist, usedlist) = parse_input(&input);
+    let part1 = part1_sol(disk);
+    let part2 = part2_sol(freelist, usedlist);
     print!("Part1: {part1}, ");
     print!("Part2: {part2}");
     println!();
