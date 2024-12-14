027f24b9e0050411ff405e7bdd3589735d9415ec
Anthony DeDominic <adedomin@gmail.com>
2024-12-14 09:43:35 -0500

    part2... whatever.

--- y2024/src/bin/2024-14.rs
+++ y2024/src/bin/2024-14.rs
@@ -1,4 +1,4 @@
-use aoc_shared::read_input_to_string;
+use aoc_shared::{array_windows, read_input_to_string, FlatVec2D};
 use itertools::Itertools;
 use std::io;
 
@@ -8,8 +8,12 @@ type Output = Vec<(Int, Int, Int, Int)>;
 fn parse_input(input: &str) -> Output {
     input
         .split(|chr: char| !chr.is_ascii_digit() && chr != '-')
-        .filter(|word| !word.is_empty())
-        .filter_map(|num| num.parse::<Int>().ok())
+        .filter_map(|num| {
+            // bool type does not have a "map" sadly.
+            (!num.is_empty())
+                .then(|| num.parse::<Int>().ok())
+                .and_then(|x| x)
+        })
         .tuples()
         .collect::<Output>()
 }
@@ -30,6 +34,8 @@ const P1_QUADS: [(Int, Int, Int, Int); 4] = [
 ];
 
 const P1_MOVE: Int = 100;
+// the distortion of the picture must be under this number????
+const P1_TOTAL_GUESS: f64 = 1f64;
 
 fn part1_sol(input: &Output) -> Int {
     let quads = P1_QUADS.map(|(xs, xe, ys, ye)| (xs..xe, ys..ye));
@@ -55,15 +61,67 @@ fn part1_sol(input: &Output) -> Int {
         .product()
 }
 
-// fn part2_sol(input: &Output) -> Int {}
+fn calc_disorder(picture: &FlatVec2D<bool>) -> f64 {
+    let (mis, set) = array_windows(&picture.0).fold((0f64, 0f64), |(mismatch, set), [l, r]| {
+        let mut mis = mismatch;
+        let mut set = set;
+        if l != r {
+            mis += 1f64;
+        }
+
+        if *l {
+            set += 1f64;
+        }
+
+        (mis, set)
+    });
+    // there has to be at least one set pixel (>0)
+    mis / set
+}
+
+fn print_tree(picture: &FlatVec2D<bool>) {
+    for y in picture.yrange() {
+        for x in picture.xrange() {
+            if picture[(x, y)] {
+                print!("@")
+            } else {
+                print!(" ")
+            }
+        }
+        println!();
+    }
+}
+
+// at each step, check the disorder.
+fn part2_sol(input: &Output) -> Int {
+    let mut points = input.clone();
+    let mut picture = FlatVec2D::<bool>::new(P1_BOUNDS_X as usize, P1_BOUNDS_Y as usize);
+    for i in 1.. {
+        picture.0.fill(false);
+        points.iter_mut().for_each(|(x, y, vx, vy)| {
+            *x = aoc_shared::wrap(*x + *vx, P1_BOUNDS_X) as isize;
+            *y = aoc_shared::wrap(*y + *vy, P1_BOUNDS_Y) as isize;
+            picture[(*x as usize, *y as usize)] = true;
+        });
+
+        let disorder = calc_disorder(&picture);
+        // literal
+        if disorder < P1_TOTAL_GUESS {
+            #[cfg(debug_assertions)]
+            print_tree(&picture);
+            return i;
+        }
+    }
+    unreachable!();
+}
 
 fn main() -> io::Result<()> {
     let input = read_input_to_string()?;
     let parsed_input = parse_input(&input);
     let part1 = part1_sol(&parsed_input);
-    // let part2 = part2_sol(&parsed_input);
+    let part2 = part2_sol(&parsed_input);
     print!("Part1: {part1}, ");
-    // print!("Part2: {part2}");
+    print!("Part2: {part2}");
     println!();
     Ok(())
 }
