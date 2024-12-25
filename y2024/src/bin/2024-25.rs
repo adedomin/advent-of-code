313aef0c8ff43992a2ac9c78a4adaf9afa05e6e9
Anthony DeDominic <adedomin@gmail.com>
2024-12-25 10:41:55 -0500

    faster...er

--- y2024/src/bin/2024-25.rs
+++ y2024/src/bin/2024-25.rs
@@ -1,57 +1,38 @@
 use aoc_shared::read_input_to_string;
-use itertools::Itertools;
 use std::io;
 
-type Int = i32;
-type Output = [Vec<[Int; PIN_CNT]>; 2];
-const KEY: usize = 0;
-const LOCK: usize = 1;
-const PIN_CNT: usize = 5;
-const LOCK_DEPTH: Int = 7;
+type Output = Vec<u64>;
+// const KEY: usize = 0;
+// const LOCK: usize = 1;
+// const PIN_CNT: usize = 5;
+// const LOCK_DEPTH: Int = 7;
+// const BITS: usize = 5 * 7;
 
 fn parse_input(input: &str) -> Output {
-    let mut ret = [vec![], vec![]];
-    let pat = |c| (c == '#') as Int;
-    input.split("\n\n").for_each(|pattern| {
-        let (t, pat) = pattern
-            .chars()
-            .filter(|&c| c == '.' || c == '#')
-            .tuples()
-            .enumerate()
-            .fold((KEY, [0; PIN_CNT]), |(t, cnt), (i, (a, b, c, d, e))| {
-                // println!("- {a} {b} {c} {d} {e} -");
-                let t = if i == 0 && (a, b, c, d, e) == ('#', '#', '#', '#', '#') {
-                    LOCK
-                } else {
-                    t
-                };
-                (
-                    t,
-                    [
-                        cnt[0] + pat(a),
-                        cnt[1] + pat(b),
-                        cnt[2] + pat(c),
-                        cnt[3] + pat(d),
-                        cnt[4] + pat(e),
-                    ],
-                )
-            });
-        ret[t].push(pat);
-    });
-    ret
+    let pat = |b| (b == b'#') as u64;
+    input
+        .split("\n\n")
+        .filter(|pat| !pat.is_empty())
+        .map(|pattern| {
+            pattern
+                .as_bytes()
+                .iter()
+                .filter(|&&b| b == b'.' || b == b'#')
+                .fold(0u64, |acc, &b| acc << 1 | pat(b))
+        })
+        .collect::<Vec<u64>>()
 }
 
-fn part1_sol([keys, locks]: Output) -> Int {
-    locks
+fn part1_sol(keys_and_locks: Output) -> u64 {
+    // we exploit the top and bottom rows differentiating the two and they *ALWAYS* self filter
+    // we basically just ask if both lock and key occupy the same space, if not, then ANDing them is always 0.
+    keys_and_locks
         .iter()
-        .map(|lock| {
-            keys.iter()
-                .filter(|key| {
-                    lock.iter()
-                        .zip(key.iter())
-                        .all(|(lp, kp)| lp + kp <= LOCK_DEPTH)
-                })
-                .count() as Int
+        .enumerate()
+        .flat_map(|(idx, kl)| {
+            keys_and_locks[..idx]
+                .iter()
+                .map(move |&kl2| ((kl & kl2) == 0) as u64)
         })
         .sum()
 }
