9da8f698553bce74160529d4a0b6e391d3720e3e
Anthony DeDominic <adedomin@gmail.com>
2025-09-16 20:59:08 -0400

    better comments......

--- y2024/src/bin/2024-12.rs
+++ y2024/src/bin/2024-12.rs
@@ -1,5 +1,4 @@
 use aoc_shared::{debug, pad_to_flat2d, read_input, FlatVec2D};
-use rustc_hash::FxHashMap;
 
 #[derive(Default, Copy, Clone, PartialEq, Eq, Debug)]
 struct X(u8);
@@ -13,26 +12,32 @@ impl From<u8> for X {
     }
 }
 
+const OUT_OF_BOUNDS: X = X(u8::MAX);
+
 type Output = FlatVec2D<X>;
-#[rustfmt::skip]
-const CARD: [(isize, isize); 4] = {
-    let (sx, sy) = (0,-1);
-    let mut card = [(sx,sy); 4];
-    let mut i = 1;
-    while i < card.len() {
-        card[i] = rot90(card[i-1]);
-        i += 1;
-    }
-    card
-};
 
 const fn rot90((x, y): (isize, isize)) -> (isize, isize) {
     (-y, x)
 }
 
-const OUT_OF_BOUNDS: X = X(b'Z' + 1);
+const CARD: [(isize, isize); 4] = {
+    let (sx, sy) = (0, -1);
+    let mut card = [(sx, sy); 4];
+    let mut i = 1;
+    while i < card.len() {
+        card[i] = rot90(card[i - 1]);
+        i += 1;
+    }
+    card
+};
 
-type ShapeBoundsSides = (usize, FxHashMap<(usize, usize), usize>, usize);
+type ShapeBoundsSides = (usize, usize, usize, Vec<(usize, usize)>);
+
+/// Get right as in clockwise 90deg from given vector dx, dy.
+fn get_right(x: usize, y: usize, dx: isize, dy: isize) -> (usize, usize) {
+    let (cdx, cdy) = rot90((dx, dy));
+    ((x as isize + cdx) as usize, (y as isize + cdy) as usize)
+}
 
 fn get_shape(
     map: &Output,
@@ -44,35 +49,46 @@ fn get_shape(
     }
 
     let mut area = 1usize;
-    let mut frontier = FxHashMap::default();
+    let mut perim = 0usize;
     let mut corners = 0usize;
+    let mut frontier = vec![];
     let mut stack = vec![(x, y)];
     let c = map[(x, y)];
+
     while let Some((x, y)) = stack.pop() {
         for (dx, dy) in CARD {
             let (nx, ny) = ((x as isize + dx) as usize, (y as isize + dy) as usize);
             let n = map[(nx, ny)];
 
             if n != c {
-                *frontier.entry((nx, ny)).or_default() += 1;
-                let (cdx, cdy) = rot90((dx, dy));
-                let (cnx, cny) = ((x as isize + cdx) as usize, (y as isize + cdy) as usize);
-                let cn = map[(cnx, cny)];
-                let (anx, any) = ((nx as isize + cdx) as usize, (ny as isize + cdy) as usize);
-                let an = map[(anx, any)];
-                debug!(
-                    "Shape {c:?}({x},{y}): n:{n:?}({nx},{ny}) cn:{cn:?}({cnx},{cny}), an:{an:?}({anx},{any}), cn != c || an == c {}",
-                    cn != c || an == c
-                );
-                corners += usize::from(cn != c || an == c);
+                perim += 1;
+
+                // Check the immediate cw 90deg rot from the frontier node.
+                // ___
+                //    | * <- What we're looking for here.
+                let cn = map[get_right(x, y, dx, dy)];
+                let is_right_edge = cn != c;
+
+                // We now check to see if we're a concave corner
+                // - Frontier -> * | * <- the node we're checking
+                //     border ->___|   <- Same shape (color)
+                let an = map[get_right(nx, ny, dx, dy)];
+                let is_concave = an == c;
+
+                if is_right_edge || is_concave {
+                    corners += 1;
+                }
+
+                frontier.push((nx, ny));
             } else if !std::mem::replace(&mut visit[(nx, ny)], true) {
-                stack.push((nx, ny));
                 area += 1;
+
+                stack.push((nx, ny));
             }
         }
     }
-    debug!("Shape {c:?}: {area}, {corners}");
-    Some((area, frontier, corners))
+    debug!("Shape {c:?}: area:{area}, perimeter:{perim}, sides:{corners}");
+    Some((area, perim, corners, frontier))
 }
 
 fn solve(map: &Output) -> (usize, usize) {
@@ -81,13 +97,12 @@ fn solve(map: &Output) -> (usize, usize) {
     let mut p2 = 0;
     let mut stack = vec![(1, 1)];
     while let Some((x, y)) = stack.pop() {
-        let Some((area, bounds, sides)) = get_shape(map, &mut visit, (x, y)) else {
+        let Some((area, perimeter, sides, frontier)) = get_shape(map, &mut visit, (x, y)) else {
             continue;
         };
-        let perimeter = bounds.values().sum::<usize>();
         p1 += area * perimeter;
         p2 += area * sides;
-        stack.extend(bounds.into_keys());
+        stack.extend(frontier);
     }
     (p1, p2)
 }
