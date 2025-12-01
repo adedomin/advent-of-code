360417337b236af918a666911f995779019b7077
Anthony DeDominic <adedomin@gmail.com>
2025-12-01 10:51:28 -0500

    someone pointed out, while it may work for me, the divisor does not work on negative wrapping rotations, which I do not have.

--- y2025/src/bin/2025-01.rs
+++ y2025/src/bin/2025-01.rs
@@ -24,8 +24,13 @@ const DIAL_START: Int = 50;
 fn solve(i: &[Int]) -> (Int, Int) {
     i.iter()
         .fold(((0, 0), DIAL_START), |((p1, p2), dial), rot| {
-            let ndial = (dial + rot).rem_euclid(DIAL_LEN);
-            let rot_around = (dial + rot).div_euclid(DIAL_LEN).abs();
+            let sum = dial + rot;
+            let ndial = sum.rem_euclid(DIAL_LEN);
+            let rot_around = if *rot < 0 {
+                (DIAL_LEN - dial - *rot) / DIAL_LEN - if dial == 0 { 1 } else { 0 } // edge-case, div returns 1 extra on 0.
+            } else {
+                sum / DIAL_LEN
+            };
             ((p1 + Int::from(ndial == 0), p2 + rot_around), ndial)
         })
         .0
