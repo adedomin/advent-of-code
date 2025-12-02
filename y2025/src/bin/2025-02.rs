bce3017bf2e8c2c9db215beaf4cfe797b075de1b
Anthony DeDominic <adedomin@gmail.com>
2025-12-02 13:17:43 -0500

    minor performance drop, but less bloaty

--- y2025/src/bin/2025-02.rs
+++ y2025/src/bin/2025-02.rs
@@ -27,35 +27,19 @@ fn solve2(i: &[(Int, Int)]) -> (Int, Int) {
                 return (p1, p2);
             }
 
-            // midpoint of decimal number
             let mid = digits / 2;
-            let midpoint = TEN.pow(mid);
-            let upper = n / midpoint;
-            let lower = n % midpoint;
-            // P1: 464464 -> 464 | 464 -> 464 == 464
-            if upper == lower {
-                return (p1 + n, p2 + n);
-            };
-
-            // P2: decimal number of n digits of 1's
-            let all_ones = (TEN.pow(digits) - 1) / 9;
-            // detect numbers like 333333 444444, 22, etc.
-            if n == n % TEN /* first digit */ * all_ones {
-                return (p1, p2 + n);
-            }
-
-            // P2: check all patterns in-between the ones we checked above.
-            // e.g. 123123123 -> 123 * 3
-            if (2..(digits / 2))
+            let is_even = digits.is_multiple_of(2);
+            if let Some(pos) = (1..mid + 1)
+                .rev()
                 .filter(|&sub| digits.is_multiple_of(sub))
-                .any(|sublen| {
+                .find(|&sublen| {
                     let pow = TEN.pow(sublen);
                     let sub = n % pow;
                     let reconstructed = (0..digits / sublen).fold(0, |acc, _| acc * pow + sub);
                     n == reconstructed
                 })
             {
-                (p1, p2 + n)
+                (p1 + n * Int::from(pos == mid && is_even), p2 + n)
             } else {
                 (p1, p2)
             }
