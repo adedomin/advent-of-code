e906bc73f73fa8fdf85b84d13e1ca200a500f7ca
Anthony DeDominic <adedomin@gmail.com>
2025-12-04 14:45:40 -0500

    2019 p1 & 2

---
+++ y2019/src/bin/2019-21.rs
@@ -0,0 +1,65 @@
+use std::{
+    collections::VecDeque,
+    io::{self, Write as _},
+};
+
+use y2019::intcode::{brk, read_intcode, IntCode};
+
+// (     we must jump if 3 tiles ahead is a hole: NOT C T because there are "islands like #.#.#
+//   AND we must land 4 tiles ahead if we are to jump: AND D T, while concerned about island
+// ) OR  we must jump if 1 tile ahead is a hole
+const P1_SPRING_SCRIPT: &str = "\
+NOT C T
+AND D T
+OR T J
+NOT A T
+OR T J
+WALK
+";
+
+// I'll be honest, I just started with P1 and just shoved instructions in til it worked.
+const P2_SPRING_SCRIPT: &str = "\
+NOT C T
+AND D T
+OR T J
+AND H J
+NOT B T
+AND D T
+OR T J
+NOT A T
+OR T J
+RUN
+";
+
+fn solve(mut program: Vec<i64>, spring_script: &str) -> i64 {
+    let mut output = 0;
+    let mut springscript = VecDeque::new();
+    write!(springscript, "{spring_script}").unwrap();
+    let mut intcode = IntCode::default();
+    loop {
+        match intcode.execute_til(&mut program, &mut springscript) {
+            Ok(out) => {
+                if out / 2i64.pow(7) > 0 {
+                    output = out;
+                } else {
+                    #[cfg(debug_assertions)]
+                    print!("{}", char::from(out as u8));
+                }
+            }
+            Err(y2019::intcode::IntCodeErr::OutOfBounds(oob)) => {
+                brk(oob, &mut program).expect("Out of Memory.")
+            }
+            Err(y2019::intcode::IntCodeErr::End) => break,
+            Err(e) => panic!("Unexpected error: {e}"),
+        }
+    }
+    output
+}
+
+fn main() -> io::Result<()> {
+    let program = read_intcode()?;
+    let part1 = solve(program.clone(), P1_SPRING_SCRIPT);
+    let part2 = solve(program, P2_SPRING_SCRIPT);
+    println!("Part1 {part1}  Part2 {part2}");
+    Ok(())
+}
