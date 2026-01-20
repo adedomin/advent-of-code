10b68f0decf45548a8b0c44fb04e4b65e4097733
Anthony DeDominic <adedomin@gmail.com>
2026-01-20 18:10:04 -0500

    clean up 2021-08

--- y2021/src/bin/2021-08.rs
+++ y2021/src/bin/2021-08.rs
@@ -1,186 +1,129 @@
 use std::io;
 
-use aoc_shared::{read_input, AoCTokenizer, Token};
+use aoc_shared::read_input_to_string;
 
-const PIPE: u8 = 0b1000_0000;
-
-fn contains_bits(arr: u32, bits: u8) -> bool {
-    let (high, mid, low) = decompose_bit_arr(arr);
-    high == bits || mid == bits || low == bits
+fn set_sixer(arr: &mut [u8; 3], bits: u8) -> Result<(), ()> {
+    for a in arr {
+        if *a == bits {
+            return Ok(());
+        } else if *a == 0 {
+            *a = bits;
+            return Ok(());
+        }
+    }
+    Err(())
 }
 
-fn decompose_bit_arr(arr: u32) -> (u8, u8, u8) {
-    (
-        (arr & 0b1111_1111) as u8,
-        ((arr & 0b1111_1111_0000_0000) >> 8) as u8,
-        ((arr & 0b1111_1111_0000_0000_0000_0000) >> 16) as u8,
-    )
-}
+fn solve(signals: &[u8], digits: &[u8]) -> u64 {
+    // 0 6 9 -> 6 bits
+    let mut sixers: [u8; 3] = [0; 3];
+    // 2 3 5 -> 5 bits, unnessary to track.
+    // let mut fivers
 
-fn set_len_bit_arr(arr: u32, value: u32) -> u32 {
-    (arr & 0b1111_1111_1111_1111_1111_1111) | value << 24
-}
-
-fn solve(segments: &[u8]) -> u64 {
-    // 0 6 9
-    let mut sixers = 0u32;
-    // 2 3 5
-    let mut fivers = 0u32;
-
-    let mut divider = 0usize;
-
-    // zero
+    // 2 bits
     let mut one = 8;
-    // two
-    // three
+    // 4 bits
     let mut four = 8;
-    // five
-    // six
+    // 3 bits.
     let mut seven = 8;
-    let eight = 0b0111_1111; // eight is always 7bits
+    // 7 bits. all set.
+    let eight = 0b0111_1111;
 
-    for (idx, &segment) in segments.iter().enumerate() {
+    for &segment in signals.iter().chain(digits) {
         match segment.count_ones() {
-            1 => divider = idx,
             2 => one = segment,
             3 => seven = segment,
             4 => four = segment,
-            5 if !contains_bits(fivers, segment) && (fivers >> 24) < 3 => {
-                let pos = fivers >> 24;
-                fivers |= (segment as u32) << (pos * 8);
-                fivers |= set_len_bit_arr(fivers, pos + 1);
-            }
-            6 if !contains_bits(sixers, segment) && (sixers >> 24) < 3 => {
-                let pos = sixers >> 24;
-                sixers |= (segment as u32) << (pos * 8);
-                sixers = set_len_bit_arr(sixers, pos + 1);
-            }
+            6 => set_sixer(&mut sixers, segment).expect("too many 6 bit segments."),
             5..=7 => (),
             _ => panic!("bad input"),
         }
     }
 
-    if one == 8 || four == 8 || seven == 8 {
+    if one == 8 || four == 8 || seven == 8 || sixers.contains(&0) {
         panic!("Not enough input");
     }
 
     // Parts:
-
-    //    __  a
+    //     a
+    //    __
     // g |_c| b
     // f |__| d
-    //    e
-    // a
-    // b
-    // c
-    // d
-    // e
-    // f
-    // g
+    //     e
 
     let a = one ^ seven;
     let ef = four ^ a ^ eight;
     let abcdg = four ^ a;
 
-    let (sixer_1, sixer_2, sixer_3) = decompose_bit_arr(sixers);
-    let nine = if (sixer_1 ^ abcdg).count_ones() == 1 {
-        sixer_1
-    } else if (sixer_2 ^ abcdg).count_ones() == 1 {
-        sixer_2
-    } else if (sixer_3 ^ abcdg).count_ones() == 1 {
-        sixer_3
-    } else {
-        panic!("Could not find nine");
-    };
+    let nine = *sixers
+        .iter()
+        .find(|&&seg| (seg ^ abcdg).count_ones() == 1)
+        .expect("Could not find nine.");
 
     let e = abcdg ^ nine;
     let f = ef ^ e;
-    let six = if sixer_1 != nine && (sixer_1 ^ abcdg ^ one).count_ones() == 3 {
-        sixer_1
-    } else if sixer_2 != nine && (sixer_2 ^ abcdg ^ one).count_ones() == 3 {
-        sixer_2
-    } else if sixer_3 != nine && (sixer_3 ^ abcdg ^ one).count_ones() == 3 {
-        sixer_3
-    } else {
-        panic!("Could not find six");
-    };
+
+    let six = *sixers
+        .iter()
+        .find(|&&seg| seg != nine && (seg ^ abcdg ^ one).count_ones() == 3)
+        .expect("Could not find six.");
 
     let b = six ^ nine ^ f;
     let d = one ^ b;
-    let zero = if sixer_1 != six && sixer_1 != nine {
-        sixer_1
-    } else if sixer_2 != six && sixer_2 != nine {
-        sixer_2
-    } else if sixer_3 != six && sixer_3 != nine {
-        sixer_3
-    } else {
-        panic!("Could not find zero");
-    };
+
+    let zero = *sixers
+        .iter()
+        .find(|&&seg| seg != six && seg != nine)
+        .expect("Could not find zero.");
 
     let c = zero ^ eight;
     let g = abcdg ^ a ^ b ^ c ^ d;
 
+    // resolve the 5 bits from parts.
     let two = a | b | c | e | f;
     let three = a | b | c | d | e;
     let five = a | c | d | e | g;
 
     let segement_digits = [zero, one, two, three, four, five, six, seven, eight, nine];
-    //    println!(
-    //        "
-    //        a = {:#09b}
-    //{:#09b} = g     b = {:#09b}
-    //        c = {:#09b}
-    //{:#09b} = f     d = {:#09b}
-    //        e = {:#09b}",
-    //        a, b, c, d, e, f, g
-    //    );
 
-    let (_, display) = segments.split_at(divider);
-    display.iter().skip(1).fold(0u64, |acc, &digit| {
-        let decoded = segement_digits.iter().position(|&d| d == digit).unwrap() as u64;
+    digits.iter().fold(0u64, |acc, &digit| {
+        let decoded = segement_digits
+            .iter()
+            .position(|&d| d == digit)
+            .expect("Digit not found.") as u64;
         acc * 10 + decoded
     })
 }
 
-fn parse(input: Vec<u8>) -> (u64, u64) {
-    let mut numcnt = 0u64;
-    let mut sum = 0u64;
-
-    let mut past_delim = false;
-    let mut parsed = vec![];
-    for token in AoCTokenizer::new(&input) {
-        match token {
-            Token::Something(digits) => {
-                numcnt += if past_delim {
-                    let digit_cnt = digits.len();
-                    if (2..=4).contains(&digit_cnt) || digit_cnt == 7 {
-                        1
-                    } else {
-                        0
-                    }
-                } else {
-                    0
-                };
-                parsed.push(digits.iter().fold(0u8, |acc, &d| acc | 1 << (d - b'a')));
-            }
-            Token::Delimiter(b'|') => {
-                parsed.push(PIPE);
-                past_delim = true;
-            }
-            Token::Newline | Token::DoubleNewline => {
-                sum += solve(&parsed);
-                parsed.clear();
-                past_delim = false;
-            }
-            Token::Space | Token::Delimiter(_) | Token::End => (),
-        }
-    }
-    (numcnt, sum)
+fn parse(input: &str) -> Vec<u8> {
+    input
+        .split_ascii_whitespace()
+        .filter(|word| *word != "|")
+        .map(|word| {
+            word.as_bytes()
+                .iter()
+                .fold(0, |acc, &d| acc | 1 << (d - b'a'))
+        })
+        .collect()
 }
 
 pub fn main() -> io::Result<()> {
-    let input = read_input()?;
-    let (p1, p2) = parse(input);
+    let input = read_input_to_string()?;
+    let sets = parse(&input);
+    // input contains 10 signal patterns | 4 digit patterns, per line.
+    assert!(
+        sets.len().is_multiple_of(14),
+        "input is malformed; missing patterns."
+    );
+    let (p1, p2) = sets.chunks_exact(14).fold((0, 0), |(p1, p2), segments| {
+        let (signal, digits) = segments.split_at(10);
+        (
+            p1 + digits.iter().fold(0, |acc, dig| {
+                acc + [2, 3, 4, 7].contains(&dig.count_ones()) as i32
+            }),
+            p2 + solve(signal, digits),
+        )
+    });
     println!("Part1 {}, Part2 {}", p1, p2);
     Ok(())
 }
